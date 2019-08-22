25ac9062fcf786e8bb0026aae69103ed03a64ea9
hpf-zzu <945928180@qq.com>
2019-08-22 22:30:40 +0800

    aa

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,66 @@
-const add = require('../main');
+const {countProducts,fetchProduct,assemble,countTotalPrice,generatedReceiptItem} = require('../main');
+// it ('should add two numbers', () => {
+//     expect(add(2, 3)).toBe(5);
+// });
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-});
+// it( 'should count codes',() => {
+//     //given
+//     const codes = ['0003','0001','0003'];
+//     //when
+//     const countedCodes = countProducts(codes);
+//     console.log(countedCodes);
+//     //then
+//     //expect(countecodes[0].count).toBe(2);
+//     expect(countedCodes[0].count).toBe(2);
+// }
+// );
+
+// it('should fetch product', () => {
+//     //given
+//     const db = [
+//         {"id": "0001", "name" : "Coca Cola", "price": 3},
+//         {"id": "0002", "name" : "Diet Coke", "price": 4}
+//     ];
+//     const code = '0001';
+//     //when
+//     const product = fetchProduct(db,code);
+//     //then
+//     expect(product[0].price).toBe(3);
+
+// });
+
+ it('validate result', () =>{
+//     var codes = generatedReceiptItems(['0003','0003','0001']);
+//     console.log("generatedReceiptItems:",codes);
+//     [
+//         {name: 'Pepsi-Cola', price: 5, count: 2},
+//         {name: 'Coca Cola', price: 3, count: 1}
+//     ]
+
+// }
+// );
+// var assembleInput = [
+//     {name: 'Pepsi-Cola', price: 5, count: 2},
+//     {name: 'Coca Cola', price: 3, count: 1}
+// ];
+// var receiptText = assemble(assembleInput,13);
+// console.log(receiptText);
+
+// var totalPrice = countTotalPrice(Input);
+// console.log(totalPrice);
+const codes = ['0003','0001','0003'];
+console.log(fetchProduct('0001'));
+var out = countProducts(codes);
+console.log(out);
+
+var out1 = generatedReceiptItem(codes);
+
+console.log(out1);
+
+var sum = countTotalPrice(out1);
+var out2 = assemble(out1,sum);
+console.log(out2);
+
+
+})
+
--- main.js
+++ main.js
@@ -1,5 +1,93 @@
-function add (left, right) {
-    return left + right;
+const db = [
+    {"id": "0001", "name" : "Coca Cola", "price": 3},
+    {"id": "0002", "name" : "Diet Coke", "price": 4},
+    {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+    {"id": "0004", "name" : "Mountain Dew", "price": 6},
+    {"id": "0005", "name" : "Dr Pepper", "price": 7},
+    {"id": "0006", "name" : "Sprite", "price": 8},
+    {"id": "0007", "name" : "Diet Pepsi", "price": 9},
+    {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+    {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+    {"id": "0010", "name" : "Fanta", "price": 12}
+]
+function countProducts(codes){
+    var map = new Map();
+    for(let index = 0; index < codes.length; index ++){
+        var code = codes[index];
+        
+        if(!map.has(code)){
+            var item = {code:code, count:1};
+            map.set(code,item);
+        }else{
+            var item = map.get(code);
+            item.count++;
+            map.set(code,item);
+    
+        }
+
+    }
+    var re = [];
+    map.forEach(function(itema){
+        re.push({
+            code:itema.code,
+        count:itema.count
+
+        })
+    
+    
+        //re.push(itema);
+    });
+    return re;
+}
+function fetchProduct(code){
+    var re = [];
+    for(let index = 0; index < db.length; index ++){
+        if(db[index].id == code){
+            re.push({price:db[index].price,
+                    name:db[index].name});
+        }
+    }
+    return re;
+}
+function generatedReceiptItem(codes){
+    var countCodes = countProducts(codes);
+    var re = [];
+    countCodes.forEach(function(item){
+        
+        var product = fetchProduct(item.code);
+        re.push({
+                "name":product[0].name,
+                "price":product[0].price,
+                "count":item.count
+
+            });
+    });
+    console.log("re",re);
+    return re;
+}  
+function countTotalPrice(Input){
+    var sum = 0;
+    Input.forEach(function(item){
+        sum += item.price * item.count;
+
+    });
+    return sum; 
+}
+function assemble(assembleInput,sum){
+    var str1 = "Receipt" + "\n";
+    var str2 = "-----------------------\n"
+    assembleInput.forEach(function(item){
+        str2 += item.name + "\t" 
+        + item.price + "\t" + item.count + "\n";
+    })
+    strout = str1 + str2 + "-------------------\n" + 
+    "Price:" + sum;
+    return strout;
+
 }
 
-module.exports = add;
+
+
+
+module.exports = {countTotalPrice,countProducts,fetchProduct,assemble,generatedReceiptItem};
+
